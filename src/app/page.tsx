5637bdb1603fb7f640595b02aea06c2a9b442cba
piyushkhurana8744 <piyushkhurana8744@gmail.com>
2024-08-11 21:06:45 +0530

    add navbar is in process

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/global/navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center flex-col">
+     <Navbar />
+    </main>
+  );
+}
